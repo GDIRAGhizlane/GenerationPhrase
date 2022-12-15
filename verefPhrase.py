e26e05efc37a68142a4fe7aa033ed2307a76c824
Ghizlae Gdira <gdira@LAPTOP-DQBLBEJB.localdomain>
2022-12-15 14:22:23 +0100

    Ajout de la fonction de la véréficatuin du  lexique

--- verefPhrase.py
+++ verefPhrase.py
@@ -16,3 +16,26 @@ genereSens()
 
 
 
+def verifierLexique():
+    colonnes = ['Mots' ,'Lemme','EtiquettePOS','Score']
+    liste_mots = pd.DataFrame(columns= colonnes)
+    liste_mots = pd.read_table('testResult.txt' , names= colonnes , sep = ' ', dtype={'Mots': str, 'Lemme': str, 'EtiquettePOS':str, 'Score':float}, header=None, index_col=None, encoding='utf8')
+    """
+    options = ['PP1CSN0', 'SP']
+
+    rslt_df = liste_mots[liste_mots['EtiquettePOS'].isin(options)]
+    print('\nResult dataframe :\n', rslt_df)
+    """
+    liste=[]
+    for i in range(0,len(liste_mots)):
+        carc = liste_mots['EtiquettePOS'][i][0]
+        if carc == 'P' or carc == 'D':
+            liste.append(liste_mots['Mots'][i])
+        if carc == 'V':
+            liste.append(liste_mots['Mots'][i])
+        if carc == 'N' or carc == 'C':
+            liste.append(liste_mots['Mots'][i])
+    print("--------------------------------------------")
+    print(liste)    
+  
+verifierLexique()
