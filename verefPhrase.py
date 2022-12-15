bbe86173f1466a1e1351bbca0a5b01d499f05da9
Ghizlae Gdira <gdira@LAPTOP-DQBLBEJB.localdomain>
2022-12-15 14:30:34 +0100

    Vérifaction de la syntaxe du phrase grammaticalement

--- verefPhrase.py
+++ verefPhrase.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 
+#fichier testResult : pour mettre les résultats de freeling
 os.system('analyze -f fr.cfg < test.txt > testResult.txt')
 print("Le fichier a été généré !!!")
 
@@ -9,24 +10,25 @@ def genereSens():
     liste_mots = pd.DataFrame(columns= colonnes)
     liste_mots = pd.read_table('testResult.txt' , names= colonnes , sep = ' ', dtype={'Mots': str, 'Lemme': str, 'EtiquettePOS':str, 'Score':float}, header=None, index_col=None, encoding='utf8')
 
-    #print(liste_mots.head())
+    print(liste_mots.head())
     print(liste_mots.shape)
     print(liste_mots)
 genereSens()
-
 
 
 def verifierLexique():
     colonnes = ['Mots' ,'Lemme','EtiquettePOS','Score']
     liste_mots = pd.DataFrame(columns= colonnes)
     liste_mots = pd.read_table('testResult.txt' , names= colonnes , sep = ' ', dtype={'Mots': str, 'Lemme': str, 'EtiquettePOS':str, 'Score':float}, header=None, index_col=None, encoding='utf8')
-    """
+    
     options = ['PP1CSN0', 'SP']
 
     rslt_df = liste_mots[liste_mots['EtiquettePOS'].isin(options)]
     print('\nResult dataframe :\n', rslt_df)
-    """
+    
     liste=[]
+
+    #Conditions pour vérifier la phrase grammaticalement
     for i in range(0,len(liste_mots)):
         carc = liste_mots['EtiquettePOS'][i][0]
         if carc == 'P' or carc == 'D':
